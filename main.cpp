d9db728924982b2a8850ff7f72b65182603d4111
KernelDash <152013203+KernelDash@users.noreply.github.com>
2025-10-30 17:54:47 +0100

    Handle invalid input and implement difficulty levels

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <random>
 #include <string>
@@ -5,24 +6,45 @@
 using u32    = uint_least32_t; 
 using engine = std::mt19937;
 
+// Ansi escape codes
+constexpr auto RED    = "\033[7;31m";
+constexpr auto GREEN  = "\033[7;32m";
+constexpr auto RESET  = "\033[0m";
+
 void clear() {
-    std::cout << "\033[2J\033[1;1H";
+  std::cout << "\033[2J\033[1;1H"; // Clear terminal
 }
 
 int main(void) {
   std::string userInput;
+  int difficulty;
 
+  // Random generator setup
   std::random_device os_seed;
   const u32 seed = os_seed();
-
   engine generator(seed);
-  std::uniform_int_distribution< u32 > distribute(1, 9);
+  std::uniform_int_distribution<u32> distribute;
 
   clear();
-  std::cout << "Math Trainer. \n Press 'q' to quit." << std::endl;
+  std::cout << "Math Trainer. (Press 'q' to quit)" << '\n';
+
+  // Get difficulty and set number range
+  std::cout << "1 = Single digit numbers\n2 = Double digit numbers\n3 = Triple digit numbers\nEnter the difficulty: ";
+  std::cin >> difficulty;
+  switch (difficulty) {
+    case 1: distribute = std::uniform_int_distribution<u32>(1, 9); break;
+    case 2: distribute = std::uniform_int_distribution<u32>(1, 99); break;
+    case 3: distribute = std::uniform_int_distribution<u32>(1, 999); break;
+    default:
+      std::cout << RED << "Invalid difficulty" << RESET << '\n';
+      exit(0);
+  }
+
+  clear();
 
   // Main loop
-  while (userInput != "q") {
+  while (true) {
+    // Generate random numbers
     int firstNum = distribute(generator);
     int secondNum = distribute(generator);
     int result = firstNum + secondNum;
@@ -32,13 +54,19 @@ int main(void) {
 
     clear();
 
-    if (stoi(userInput) != result)
-      std::cout << "\033[7;31mWrong. Right answer: " << result << "\033[0m" << std::endl;
+    if (userInput == "q") {
+      std::cout << "Bye!\n";
+      break;
+    }
+    // Check if input is numeric
+    if (!all_of(userInput.begin(), userInput.end(), isdigit)) {
+      std::cout << RED << "Invalid input" << RESET << '\n';
+      continue;
+    }    
+    if (stoi(userInput) == result)
+      std::cout << GREEN << "Correct!" << RESET << '\n';
     else
-      std::cout << "\033[7;32mCorrect!\033[0m" << std::endl;
+      std::cout << RED << "Wrong. Right answer: " << result << RESET << '\n';
   }
-
   return 0;
 }
-
-// std::cout << distribute(generator) << std::endl;
